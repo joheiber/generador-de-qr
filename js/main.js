52020c5b8c3a1fe66fef2d461a4e1c479fc1e03c
joheiber almao <joheiberalmao1214@gmail.com>
2024-10-10 18:30:06 -0500

    termine el proyecto

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     $("#qrcode").empty();
-    
+
 
     $("#qr-form").on("submit", function (event) {
         event.preventDefault();
@@ -37,6 +37,43 @@ $(document).ready(function () {
             })
         }
     });
+
+    $("#download-btn").on("click", function () {
+
+        const canvas = $("#qr-container canvas")[0]
+        const img = canvas.toDataURL("image/png")
+
+        var link = document.createElement('a');
+        link.href = img;
+        link.download = 'qr-code.png';
+        link.click();
+    })
+    $("#share-btn").on("click", function () {
+        const canvas = $("#qr-container canvas")[0]
+        canvas.toBlob(function (blob) {
+            const item = new ClipboardItem({ "image/png": blob });
+            navigator.clipboard.write([item]).then(function () {
+                Swal.fire({
+                    title: "se a opiado en portapapales",
+                    color: "#F2F5F9",
+                    background: "#364153",
+                    confirmButtonColor: "#3662E3",
+                    toast: true,
+                    position: "bottom-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.onmouseenter = Swal.stopTimer;
+                        toast.onmouseleave = Swal.resumeTimer;
+                    }
+
+                })
+            }, function (err) {
+                console.error("No se pudo copiar la imagen: ", err);
+            });
+        });
+    })
 
     function esValidaURL(url) {
         try {
