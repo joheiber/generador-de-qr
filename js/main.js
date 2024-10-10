f7aa1b0c0dd8de2d5f3b2fbbaf7c064de6396ada
joheiber almao <joheiberalmao1214@gmail.com>
2024-10-10 15:50:22 -0500

    termine la generacion de qr y la maquetacion del parte del los qr

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
 $(document).ready(function () {
 
+    $("#qrcode").empty();
+    
+
     $("#qr-form").on("submit", function (event) {
         event.preventDefault();
-        url =  $('#qr-input').val()
+        url = $('#qr-input').val()
 
-        if (url !== "" ) {
-            if (esValidaURL(url) ) {
-                
+        if (url !== "") {
+            if (esValidaURL(url)) {
+                $("#qrcode").empty();
+                var qrcode = new QRCode(document.getElementById("qr-code"), {
+                    text: `${url}`,
+                    width: 128,
+                    height: 128,
+                    colorDark: "#000000",
+                    colorLight: "#ffffff",
+                    correctLevel: QRCode.CorrectLevel.H
+                });
+                $("#div-container").show()
+                $("#qr-tablero").hide()
             } else {
-                
+                Swal.fire({
+                    title: "URL invalido!",
+                    color: "#F2F5F9",
+                    background: "#364153",
+                    confirmButtonColor: "#3662E3",
+                })
             }
         } else {
             Swal.fire({
-                title: "Auto close alert!",
-                color:"#F2F5F9",
-                background:"#364153",
-                confirmButtonColor:"#3662E3",
-            });
+                title: "URL invalido!",
+                color: "#F2F5F9",
+                background: "#364153",
+                confirmButtonColor: "#3662E3",
+            })
         }
     });
 
